b4185dbae47fa52d20b307ca30c5ab43e4038aea
Enesoruc69 <orucenes935@gmail.com>
2025-03-03 15:48:38 +0300

    deneme geri alındı

--- fittrack/src/main/java/org/example/controller/ClientController.java
+++ fittrack/src/main/java/org/example/controller/ClientController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/clients1")
+@RequestMapping("/api/clients")
 public class ClientController {
 
     private final ClientService clientService;
